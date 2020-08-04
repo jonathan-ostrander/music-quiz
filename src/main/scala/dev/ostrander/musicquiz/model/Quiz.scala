5233809e2a3437cccc9d4dfaea8a46b6536e365d
Jonathan Ostrander <jonathanost@gmail.com>
2020-08-04 11:20:43 -0400

    use jaccard similarity

--- src/main/scala/dev/ostrander/musicquiz/model/Quiz.scala
+++ src/main/scala/dev/ostrander/musicquiz/model/Quiz.scala
@@ -5,14 +5,16 @@ import scala.util.Random
 import spray.json.DefaultJsonProtocol._
 import spray.json.JsonFormat
 import spray.json.enrichString
+import org.apache.commons.text.similarity.JaccardSimilarity
+
 
 case class Song(
   artist: String,
   song: String,
   preview: String,
 ) {
-  def isArtist(value: String): Boolean = true
-  def isTitle(value: String): Boolean = true
+  def isArtist(value: String): Boolean = Quiz.isCorrect(artist, value)
+  def isTitle(value: String): Boolean = Quiz.isCorrect(song, value)
 }
 
 object Quiz {
@@ -21,4 +23,8 @@ object Quiz {
   val songs: List[Song] = Source.fromResource("songs.json").getLines.mkString("\n").parseJson.convertTo[List[Song]]
 
   def random(n: Int): List[Song] = Random.shuffle(songs).take(n)
+
+  private[this] val jaccard = new JaccardSimilarity()
+  private[this] val threshold: Double = 0.69
+  def isCorrect(answer: String, guess: String): Boolean = jaccard(answer.toLowerCase(), guess.toLowerCase()) >= threshold
 }
