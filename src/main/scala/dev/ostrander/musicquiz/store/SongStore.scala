e4f2ff2d328574d80d9611321f71f5075f077292
Jonathan Ostrander <jonathanost@gmail.com>
2020-09-19 19:57:56 -0400

    use new model with ids

--- src/main/scala/dev/ostrander/musicquiz/store/SongStore.scala
+++ src/main/scala/dev/ostrander/musicquiz/store/SongStore.scala
@@ -10,30 +10,22 @@ import scala.concurrent.Future
 import spray.json.enrichString
 
 trait SongStore {
-  def saveSongs(songs: List[Song]): Future[Unit]
-
   def getRandomSongs(n: Int): Future[List[Song]]
 }
 
 object SongStore {
-  private[this] case class Songs(song: String) {
+  private[this] case class QuizSongs(
+    id: String,
+    song: String,
+  ) {
     def toSong: Song = song.parseJson.convertTo[Song]
-  }
-  private[this] object Songs {
-    def fromSong(song: Song): Songs = Songs(songFormat.write(song).compactPrint)
   }
 
   def apply(ctx: PostgresAsyncContext[SnakeCase])(implicit executionCtx: ExecutionContext): SongStore = new SongStore {
     import ctx._
 
-    def runBatch(songs: List[Songs]): Future[RunBatchActionResult] =
-      run(liftQuery(songs).foreach(song => query[Songs].insert(song)))
-
-    override def saveSongs(songs: List[Song]): Future[Unit] =
-      runBatch(songs.map(Songs.fromSong)).map(_ => ())
-
     override def getRandomSongs(n: Int): Future[List[Song]] =
-      run(infix"${query[Songs]} ORDER BY RANDOM() LIMIT ${lift(n)}".as[EntityQuery[Songs]])
+      run(infix"${query[QuizSongs]} ORDER BY RANDOM() LIMIT ${lift(n)}".as[EntityQuery[QuizSongs]])
         .map(r => r.map(_.toSong))
   }
 }
